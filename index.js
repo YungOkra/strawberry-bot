4ca84b448b06e05c68750a47076fb8770da34a57
Jorge Matamoros <matamorosj343@gmail.com>
2020-12-15 19:49:30 -0500

    baby steps, send a single tweet

---
+++ index.js
@@ -0,0 +1,15 @@
+const Twitter = require('twitter');
+require('dotenv').config()
+
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
+
+  client.post('statuses/update', {status: 'gang 🍓😈'},  function(error, tweet, response) {
+    if(error) throw error;
+    console.log(tweet);  // Tweet body.
+    console.log(response);  // Raw response object.
+  });
